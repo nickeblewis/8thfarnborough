1f333eb56ea445663ed1f35d37c4d0bd2186848b
Nick Lewis <nickeblewis@gmail.com>
2013-04-28 18:12:31 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Farnborough8th::Application.config.secret_token = '40695eb1434df4748b4fbf12c30678817354a163861adba4c73602665766343497e16b4848ec1250a0ab879dd0cbe93e3234c9382e615d7a7855a76001510538'
